08ce717959abec78c46c0fe190dd90f398460fe8
Isabel Lee <ijl0322@gmail.com>
2015-12-22 11:34:10 +0800

    change color

--- clock.py
+++ clock.py
@@ -9,10 +9,12 @@ window = pygame.display.set_mode((600,600))
 pygame.display.set_caption("Clock")
 
 WHITE = (255,255,255)
-BLUE = (0,0,255)
-RED = (255,0,0)
-GREEN = (0,255,0)
-PURPLE = (150,0,150)
+BLUE = (175,238,238)
+GREEN = (180,255,180)
+PURPLE = (180,180,250)
+BROWN = (205,175,149)
+DARK_BROWN = (139,69,19)
+
 myfont = pygame.font.SysFont(None, 40)
 
 def updateTime():
@@ -20,8 +22,6 @@ def updateTime():
     hour = now.hour % 12
     minute = now.minute
     second = now.second
-    #hour = 9
-    #minute = 30
     h_angle = pi*(1.0/6.0)*((hour+9)%12)
     m_angle = pi*(1.0/6.0)*(((minute/5.0)+9)%12)
     s_angle = pi*(1.0/6.0)*(((second/5.0)+9)%12)
@@ -29,7 +29,7 @@ def updateTime():
 
 def addText():
     for i in range(1,13):
-        text = myfont.render(str(i), True, RED)
+        text = myfont.render(str(i), True, DARK_BROWN)
         textRect = text.get_rect()
         t_angle = pi*(1.0/6.0)*((i+9)%12)  
         textRect.centerx = int(300 + 230*cos(t_angle))
@@ -41,13 +41,13 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-    h_angle, m_angle, s_angle = updateTime()
 
+    h_angle, m_angle, s_angle = updateTime()
             
     window.fill(WHITE)
 
-    pygame.draw.circle(window,BLUE,(300,300),200)
-    pygame.draw.circle(window, RED, (int(300 + 200*cos(h_angle)), int(300 + 200*sin(h_angle))), 20)
+    pygame.draw.circle(window,BROWN,(300,300),200)    
+    pygame.draw.circle(window, BLUE, (int(300 + 200*cos(h_angle)), int(300 + 200*sin(h_angle))), 20)
     pygame.draw.circle(window, GREEN, (int(300 + 200*cos(m_angle)), int(300 + 200*sin(m_angle))), 15)
     pygame.draw.circle(window, PURPLE, (int(300 + 200*cos(s_angle)), int(300 + 200*sin(s_angle))), 10)
     addText()
